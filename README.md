7ec2bfd0ea0350818c5ea9ca1d3038bf54f6b0b5
patcharapon-nat <patcharapon.nat@truedigital.com>
2024-01-24 15:56:14 +0700

    add server name

--- README.md
+++ README.md
@@ -2,30 +2,30 @@
 
 CREATE TABLE view_schema_check.input_trigger
 (
-
-    `interface_number` String,
-
-    `TotalRecords` Int64,
-
-    `GroupedDateTime` DateTime('Asia/Bangkok'),
-
-    `TimeStamp` DateTime DEFAULT toDateTime(now(),
-
-'Asia/Bangkok')
+`server_number` String,
+`interface_number` String,
+`TotalRecords` Int64,
+`GroupedDateTime` DateTime('Asia/Bangkok'),
+`TimeStamp` DateTime DEFAULT toDateTime(now(),'Asia/Bangkok')
 )
 ENGINE = MergeTree
 ORDER BY GroupedDateTime
 SETTINGS index_granularity = 8192;
 
-SELECT
-interface_number,
-COUNT(_) AS TotalRecords,
-DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 _ 1, 0) AS GroupedDateTime,
-getdate() as TimeStamp
-FROM
-vfinindb.dbo.INPUT_TRIGGER
-GROUP BY
-interface_number,
-DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 \* 1, 0)
-ORDER BY
-GroupedDateTime;
+---
+
+CREATE TABLE view_schema_check.input_trigger
+(
+
+    `server_number` String,
+
+    `interface_number` String,
+
+    `timestamp` DateTime,
+
+    `TineStamp` DateTime,
+
+)
+ENGINE = MergeTree
+ORDER BY GroupedDateTime
+SETTINGS index_granularity = 8192;
--- mssql.py
+++ mssql.py
@@ -8,20 +8,31 @@ client = clickhouse_connect.get_client(host='10.99.5.244', username='default', p
 def input_data(server_input):
     conn = pymssql.connect(server=server_input, user='dmp_prd', password=d.user[0]['password'], database='vfinindb')
     cursor = conn.cursor()
-
+    # print(server_input)
+    # query = f"""
+    # SELECT 
+    #     interface_number,
+    #     COUNT(*) AS TotalRecords,
+    # DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 * 1, 0) AS GroupedDateTime,
+    # getdate() as TimeStamp
+    # FROM 
+    #     vfinindb.dbo.INPUT_TRIGGER
+    # GROUP BY 
+    # interface_number,
+    #     DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 * 1, 0)
+    # ORDER BY 
+    #     GroupedDateTime;
+    # """
     query = f"""
-    SELECT 
-        interface_number,
-        COUNT(*) AS TotalRecords,
-    DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 * 1, 0) AS GroupedDateTime,
-    getdate() as TimeStamp
-    FROM 
-        vfinindb.dbo.INPUT_TRIGGER
-    GROUP BY 
-    interface_number,
-        DATEADD(MINUTE, DATEDIFF(MINUTE, 0, [timestamp]) / 1 * 1, 0)
-    ORDER BY 
-        GroupedDateTime;
+        SELECT 
+            '{server_input}' as server_name,
+            interface_number,
+            timestamp,
+            getdate() as TimeStamp
+        FROM 
+            vfinindb.dbo.INPUT_TRIGGER
+        order by
+        	3 DESC ;
     """
     cursor.execute(query)
     df1 = pd.DataFrame(cursor.fetchall())
